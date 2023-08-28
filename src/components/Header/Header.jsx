3aa2172636edf93c439510812e75b5e0d04a987f
dzoric1 <Gerasimov2099@mail.ru>
2023-08-28 23:02:11 +0800

    realized header and navigation

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,19 @@
 import { Link } from "react-router-dom";
 import './Header.css';
 import headerLogo from '../../assets/images/logo.svg';
-
+import NavigationAuth from "../NavigationAuth/NavigationAuth";
+import Navigation from "../Navigation/Navigation";
 
 
 const Header = () => {
   return (
     <header className="header">
       <div className="header__container">
-        <Link to="/" className="header__link">
+        <Link to="/">
           <img className="header__logo" src={headerLogo} alt="логотип" />
         </Link>
+        {/* <NavigationAuth /> */}
+        <Navigation />
       </div>
     </header>
   );
