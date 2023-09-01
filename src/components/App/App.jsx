6ec9c7e1adc33a744ab618d4a40ba35932072a5e
dzoric1 <Gerasimov2099@mail.ru>
2023-09-01 20:11:23 +0800

    realized AboutMe component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,3 +1,4 @@
+import AboutMe from '../AboutMe/AboutMe';
 import AboutProject from '../AboutProject/AboutProject';
 import Header from '../Header/Header';
 import Promo from '../Promo/Promo';
@@ -12,6 +13,7 @@ function App() {
       <Promo />
       <AboutProject />
       <Techs />
+      <AboutMe />
     </>
   );
 }
