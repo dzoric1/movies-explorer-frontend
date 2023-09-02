67b69d9cddc6b9a7eedeebea61e81e00bc0c3a8f
dzoric1 <Gerasimov2099@mail.ru>
2023-09-02 18:16:02 +0800

    realized search

---
+++ src/components/FilterCheckbox/FilterCheckbox.jsx
@@ -0,0 +1,15 @@
+import './FilterCheckbox.css'
+
+const FilterCheckbox = ({ text }) => {
+  return (
+    <div className='filter-сheckbox'>
+      <label className='filter-сheckbox__label' htmlFor='filtered-checkbox'>
+        <input className='filter-сheckbox__input' type="checkbox" id='filtered-checkbox' />
+        <span className='filter-сheckbox__circle'></span>
+      </label>
+      {text && <span className="switcher__text">{text}</span>}
+    </div>
+  )
+}
+
+export default FilterCheckbox;
--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,12 +1,13 @@
 import './Movies.css';
 import Header from '../Header/Header';
+import Search from '../SearchForm/Search';
 import Footer from '../Footer/Footer';
 
 const Movies = () => (
   <>
     <Header />
     <main>
-
+      <Search />
     </main>
     <Footer />
   </>
