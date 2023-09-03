029a3322053e5201d8357fa084a0b33a38b5c463
dzoric1 <Gerasimov2099@mail.ru>
2023-09-03 22:45:52 +0800

    realized profile

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import Main from '../Main/Main';
 import Movies from '../Movies/Movies';
 import SavedMovies from '../Movies/Movies';
+import Profile from '../Profile/Profile';
 import './App.css';
 
 
@@ -11,6 +12,7 @@ function App() {
       <Route path='/' element={<Main />} />
       <Route path='/movies' element={<Movies />} />
       <Route path='/saved-movies' element={<SavedMovies />} />
+      <Route path='/profile' element={<Profile />} />
     </Routes>
   );
 }
---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,50 @@
+import Header from "../Header/Header";
+import './Profile.css'
+
+const Profile = () => {
+  return (
+    <>
+      <Header />
+      <main className="profile container">
+        <h1 className="profile__title">Привет, Виталий!</h1>
+        <form className="profile__form">
+          <div className="profile__fields">
+            <label className="profile__field">
+              <h3 className="profile__input-label">Имя</h3>
+              <input
+                className='profile__input'
+                type="text"
+                placeholder="Имя"
+                name="name"
+                minLength="2"
+                maxLength="30"
+                required
+              />
+              <span className="profile__error">12312fghfghfgh31</span>
+            </label>
+            <label className="profile__field">
+              <h3 className="profile__input-label">E-mail</h3>
+              <input
+                className='profile__input'
+                type="email"
+                placeholder="E-mail"
+                name="email"
+                required
+              />
+              <span className="profile__error">123123</span>
+            </label>
+          </div>
+          <div className="profile__buttons">
+            <button className="profile__button hover-opacity" type="button">Редактировать</button>
+            <button className="profile__button profile__button_type_logout hover-opacity" type="button">Выйти из аккаунта</button>
+            {/* <p className="profile__buttons-error">При обновлении профиля произошла ошибка.</p>
+            <button className="profile__button profile__button_type_save hover-opacity">Сохранить</button> */}
+          </div>
+        </form>
+      </main>
+    </>
+
+  );
+}
+
+export default Profile;
