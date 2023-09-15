9899a2804797e6f3878f80184b71970251ecf893
dzoric1 <Gerasimov2099@mail.ru>
2023-09-15 23:49:45 +0800

    fix review layout remarks

--- src/components/Promo/Promo.jsx
+++ src/components/Promo/Promo.jsx
@@ -6,7 +6,7 @@ const Promo = () => {
     <section className="promo">
       <div className='promo__container container'>
         <div className="promo__content">
-          <h1 className="promo__title">Учебный проект студента факультета Веб-разработки.</h1>
+          <h1 className="promo__title">Учебный проект студента факультета <nobr>Веб-разработки</nobr>.</h1>
           <p className="promo__subtitle">Листайте ниже, чтобы узнать больше про этот проект и его создателя.</p>
           <a className="promo__link" href="#about">Узнать больше</a>
         </div>
